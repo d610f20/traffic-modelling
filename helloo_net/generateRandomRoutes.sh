b9098968d2cd590fe810e430bff96b6b87c312a3
NicEastvillage <nicoesterby@gmail.com>
2020-03-05 14:14:40 +0100

    Added another bus.

--- helloo_net/generateRandomRoutes.sh
+++ helloo_net/generateRandomRoutes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-"$SUMO_HOME"/tools/randomTrips.py -n helloo.net.xml --route-file=helloo.rou.xml -e 2000 --period 3 --persontrips --trip-attributes "modes=\"public car\"" --additional-file helloo.add.xml
+"$SUMO_HOME"/tools/randomTrips.py -n helloo.net.xml --route-file=helloo.rou.xml -e 2200 --period 3 --persontrips --trip-attributes "modes=\"public car\"" --additional-file helloo.add.xml
 
 sleep 1
