db1c09e5f25356ffcd0b32548d5942125c49cf18
NicEastvillage <nicoesterby@gmail.com>
2020-03-05 13:35:04 +0100

    Added more crossings.

--- helloo_net/generateRandomRoutes.sh
+++ helloo_net/generateRandomRoutes.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 "$SUMO_HOME"/tools/randomTrips.py -n helloo.net.xml --route-file=helloo.rou.xml -e 2000 --period 3 --persontrips --trip-attributes "modes=\"public car\"" --additional-file helloo.add.xml
+
+sleep 1
